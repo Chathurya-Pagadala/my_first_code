cd1ebd4257362739aea8f633b5fa6df3d8d8a442
Chathurya-Pagadala <chathuryapagadala1610@gmail.com>
2026-02-23 18:59:20 +0530

    about me

--- second-test-code.py
+++ second-test-code.py
@@ -1,6 +1,10 @@
-name = "Chathurya"
+name = "Chathurya."
 age = 21
-city = "Tirupati"
+city = "Tirupati."
+study = "final year B.Tech in CSE."
+goal = "Aspiring Cybersecurity Engineer | AI Enthusiast."
 print("My name is", name)
-print("Age -", age)
+print("Age:", age)
 print("Settled near", city)
+print("Currently pursuing my", study)
+print("My goal is to become", goal)
